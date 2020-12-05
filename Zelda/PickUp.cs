017ccc9c186a2c586bef22993cd648e2d96fea0c
smartpenguins <70032642+smartpenguins@users.noreply.github.com>
2020-12-05 01:02:06 -0800

    Added Zelda like Pot/Jar item Pick Up Script

---
+++ Zelda/PickUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdSpot;
+    public LayerMask pickUpMask;
+    public GameObject destroyEffect;
+    public Vector3 Direction { get; set; }
+    private GameObject itemHolding;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (itemHolding)
+            {
+                itemHolding.transform.position = transform.position + Direction;
+                itemHolding.transform.parent = null;
+                if (itemHolding.GetComponent<Rigidbody2D>())
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+                itemHolding = null;
+            }
+            else
+            {
+                Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .4f, pickUpMask);
+                if (pickUpItem)
+                {
+                    itemHolding = pickUpItem.gameObject;
+                    itemHolding.transform.position = holdSpot.position;
+                    itemHolding.transform.parent = transform;
+                    if (itemHolding.GetComponent<Rigidbody2D>())
+                        itemHolding.GetComponent<Rigidbody2D>().simulated = false;
+                }
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (itemHolding)
+            {
+                StartCoroutine(ThrowItem(itemHolding));
+                itemHolding = null;
+            }
+        }
+    }
+
+    IEnumerator ThrowItem(GameObject item)
+    {
+        Vector3 startPoint = item.transform.position;
+        Vector3 endPoint = transform.position + Direction * 2;
+        item.transform.parent = null;
+        for (int i = 0; i < 25; i++)
+        {
+            item.transform.position = Vector3.Lerp(startPoint, endPoint, i * .04f);
+            yield return null;
+        }
+        if (item.GetComponent<Rigidbody2D>())
+            item.GetComponent<Rigidbody2D>().simulated = true;
+        Instantiate(destroyEffect, item.transform.position, Quaternion.identity);
+        Destroy(item);
+    }
+
+}
